8f1aede160601b9b5eb27b413bbab1b0f0119fd4
Gaston Mastrapasqua <gastimastra@gmail.com>
2024-05-25 14:43:18 -0300

    feat: scale techs and tools

--- frontend/src/app/pages/knowledge/technology/technology.component.ts
+++ frontend/src/app/pages/knowledge/technology/technology.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { TechnologyKnowledge } from '../../../models/technology-knowledge';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'technology',
   standalone: true,
-  imports: [],
+  imports: [ NgClass ],
   templateUrl: './technology.component.html',
   styleUrl: './technology.component.css'
 })
 export class TechnologyComponent {
   @Input() technologyKnowledge: TechnologyKnowledge;
-  
+  @Input() scale: boolean = false;
+  @Input() tooltip: string;
   
 }
