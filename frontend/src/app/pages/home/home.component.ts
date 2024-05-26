dee87fcec1e9b79ce70d4694965b7e5daefcae40
Gaston Mastrapasqua <gastimastra@gmail.com>
2024-05-26 12:06:16 -0300

    feat: temporal home

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+
 
 }
